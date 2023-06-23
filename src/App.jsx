12181386ad7e922f701cd0811d3ce4dbcd20b645
unknown <@forsythokoeguale01gmail.com>
2023-06-23 23:10:23 +0100

    about update

--- src/App.jsx
+++ src/App.jsx
@@ -12,13 +12,9 @@ function App() {
     <div className="text-[#D3D0D0] font-light">
       <SideNav />
       <div className="m-auto w-[90%]">
-        <header className="header h-[85vh]">
-          <div className="space-y-2 flex flex-col justify-center h-[80vh] xl:h-[50vh] text-center xl:text-left">
+        <header className="header h-[80vh]">
+          <div className="space-y-2 flex flex-col justify-center h-[70vh] xl:h-[50vh] text-center xl:text-left">
             <h1 className=" text-3xl xl:text-6xl font-semibold tracking-widest text-[#F5EB6D uppercase">Forsyth Okoeguale.</h1>
-            {/* <p className="text-2xl leading-7">
-              A <span className='text-[#764ABC'>Front-End Developer</span>  <br />
-              and <span className='text-[#F5EB6D'>solutions</span> <span className='text-[#ED8554'>architec </span>
-            </p> */}
             <h1 className="tracking-wider ">A Front-End Developer</h1>
           
             <div className="">
--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -8,13 +8,13 @@ const About = () => {
                     Making the Web more <br /> <span className='text-[#ED8554'>Beautiful,</span>
                     <span className='text-[#764ABC'> Inituitive</span> and <br /> <span className='text-[#F5EB6D'> Accessible.</span>
                 </h1> */}
-                <div className="flex items-end gap-5">
-                    <h1 className="text-3xl font-semibol tracking-wider leading-8">
+                <div className="flex items-center gap-5">
+                    {/* <h1 className="text-3xl font-semibol tracking-wider leading-8">
                         Making the Web more <br /> <span className='text-[#ED8554'>Beautiful,</span>
                         <span className='text-[#764ABC'>Inituitive</span> and <br /> <span className='text-[#F5EB6D]'> Accessible.</span>
-                    </h1>
-                {/* <h1 className="font-semibold text-3xl tracking-wider">About</h1> */}
-                {/* <p className="border w-[40%] "></p> */}
+                    </h1> */}
+                <h1 className="font-semibold text-3xl tracking-wider">About</h1>
+                <p className="border w-[40%] "></p>
                 </div>
                 <p className="text-justify tracking-wide leading-6 font-light text-sm">
                     I'm a highly skilled and experienced front-end developer with a passion for creating visually appealing and user-friendly websites. With [X] years of industry experience, I have a strong command of HTML, CSS, and JavaScript, along with expertise in modern frameworks like React and Vue.js. I am committed to delivering high-quality code, pixel-perfect designs, and seamless user experiences. Let's collaborate to bring your web project to life!
